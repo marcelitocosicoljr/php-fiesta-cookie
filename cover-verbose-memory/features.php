7c9f5f6ba15249c9571c9ce9df63a3689d0da43c
Morpheus <51713799+LittleBolt00816@users.noreply.github.com>
2021-10-12 17:32:49 +0800

    inc directory done

---
+++ cover-verbose-memory/features.php
@@ -0,0 +1,18 @@
+<?php require 'inc/header.php'; ?>
+
+<main role="main" class="inner cover">
+        <h1 class="cover-heading">This is Features.</h1>
+        <p class="lead">Feature is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Feature template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        </div>
+      </footer>
+    </div>
+
+<?php require 'inc/footer.php'; ?>
